876f4300ebd9b8112ec22352effd639b333546eb
Zheng Zhiwen <zhengzhw1202@gmail.com>
2026-07-03 15:19:20 +0800

    feat(phase3a): 前端脚手架(Vite+Vue3+TS+Naive UI+Pinia+Router)

---
+++ frontend/src/router/index.ts
@@ -0,0 +1,8 @@
+import { createRouter, createWebHistory } from 'vue-router'
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [{ path: '/', component: { template: '<div>loading…</div>' } }],
+})
+
+export default router
